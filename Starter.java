cb400ac1bb958082ab6d982af165cf2cf316e757
MorgannSchartow <999976653@student.baisd.net>
2023-02-10 08:53:44 -0500

    cry

--- Starter.java
+++ Starter.java
@@ -1,35 +1,53 @@
 import java.util.Scanner;
 
 public class Starter {
-    public static void main(String args[]) {
+    public static int main(String args[]) {
         // Variables and scanner
-        int countnum = 0; 
+         int userin = 0; 
         Scanner keyboard = new Scanner(System.in); 
         // Ask the user, "How high do you want to count?" and get their input
         System.out.println("How high do you want to count?");
-         countnum = keyboard.nextInt();
+         userin = keyboard.nextInt();
         // Validate the input using a method
             // If the input is not valid, ask the user,
             // "That number is not valid. \nHow high do you want to count?"
-            
-        
+            System.out.println("That number is not valid. \nHow high do you want to count?");
+            userin = keyboard.nextInt();
+            keyboard.close();
         // Use a method to output the solution
     
 
     // Create a method that validates the input and returns true if valid
-     public static Integer validate(countnum ) {
-         if()
-            
+     public static int inValidator(userin) { 
+         int num = 0;
+        if(num == 5 || num == 3){
+            num = userin;
+            return num; 
         }
+         }
+            
+        
             // Create a method that outputs the following for each number from 1
             // to whatever the user entered.
+        public static int outputs(int num){
+            if (num/5) {
+                System.out.println("BAISD");
+            } else if(num/3){
+                System.out.println("is awesome!");
+            }else if(num/3 && num/5){
+                System.out.println("BAISD is awesome!");
+            }
+                
+            }
+            // If the number is evenly divisible by 5, output, "BAISD "
+            // If the number is evenly divisible by 3, output, "is awesome!"
+            // If the number is evenly divisible by 3 and 5, output, "BAISD is awesome!"
+            
+        }            
+    }
+        }
 
-    // If the number is evenly divisible by 5, output, "BAISD "
-    // If the number is evenly divisible by 3, output, "is awesome!"
-    // If the number is evenly divisible by 3 and 5, output, "BAISD is awesome!"
 
-}
-}
 
 // Ways to stand out:
 
