bd5a4018304bcbfdd1530a741208bca8ad7d1a2c
ahilliker7 <000059710@student.baisd.net>
2023-02-10 14:15:20 -0500

    print & modulus

--- Starter.java
+++ Starter.java
@@ -1,70 +1,64 @@
 import java.util.Scanner;
 
 public class Starter {
+    /**
+     * @param args
+     */
     public static void main(String args[]) {
         // Variables and scanner
         Scanner keyboard = new Scanner(System.in);
         int number;
+        String output = "";
         // Ask the user, "How high do you want to count?" and get their input
         System.out.println("How high do you want to count?");
         number = keyboard.nextInt();
         // Validate the input using a method
         // If the input is not valid, ask the user,
-            if (number > 15 || number < 1)
-            {
-                System.out.println("That number is not valid. \nHow high do you want to count?");
-                return;
-            }
 
+    
         // Use a method to output the solution
-    }
+        int num = 1 * number;
+        System.out.println(num);
+        System.out.println(output);
+        number++;
+    
 
     // Create a method that validates the input and returns true if valid
     
     // Create a method that outputs the following for each number from 1
     // to whatever the user entered.
-    public array number[];
+    number = parse.Boolean (number);
+    while (number >= 1)
     {
-        String[] number;
-        number[1] = "1";
-        number[2] = "2";
-        String output;
-        number[3] = output;
-        number[4] = "4";
-        number[5] = output;
-        number[6] = output;
-        number[7] = "7";
-        number[8] = "8";
-        number[9] = output;
-        number[10] = output;
-        number[11] = "11";
-        number[12] = output;
-        number[13] = "13";
-        number[14] = "14";
-        number[15] = output;
+        number == true;
+        return;
     }
-
 
     // If the number is evenly divisible by 5, output, "BAISD "
     // If the number is evenly divisible by 3, output, "is awesome!"
     // If the number is evenly divisible by 3 and 5, output, "BAISD is awesome!"
-    if (number/5)
+
+    if (num%5 == 0)
     {
-        String output = "BAISD";
+        output = "BAISD";
     }
-    
-    if (number/3)
+    else if (num%3 == 0)
     {
-        String output = "is awesome!";
+        output = "is awesome!";
+    }
+    else if (num%3 == 0 && num%5 == 0)
+    {
+        output = "BAISD is awesome!";
+    }
+    else
+    {
+        num++;
     }
 
-    if (number/3 && number/5)
-    {
-        String output = "BAISD is awesome!";
-    }
+    keyboard.close();
+}
+}
 
-}
-}
 
 
 // Ways to stand out:
