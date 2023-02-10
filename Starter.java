e2faa954418009612af1dccbe28dc5e20815fb82
CamreronO <112653776+CamreronO@users.noreply.github.com>
2023-02-10 13:57:42 -0500

    Update Starter.java

--- Starter.java
+++ Starter.java
@@ -1,26 +1,55 @@
+
 import java.util.Scanner;
+
+import javax.management.modelmbean.ModelMBean;
+
+
 
 public class Starter {
     public static void main(String args[]) {
         // Variables and scanner
-
+        boolean count;
+           Scanner keyboard = new Scanner(System.in);
         // Ask the user, "How high do you want to count?" and get their input
+         System.out.println("How high do you want to count?");
+            count = keyboard.nextBoolean();
 
-        // Validate the input using a method
+        // Validate the input using a method  
+        
         // If the input is not valid, ask the user,
-        // "That number is not valid. \nHow high do you want to count?"
+        if (count) {
 
+        // "That number is not valid. \nHow high do you want to count?"
+          System.out.print("That number is not valid. \nHow high do you want to count");
+        }
         // Use a method to output the solution
+    
     }
 
     // Create a method that validates the input and returns true if valid
-
+       public static void input(boolean count) {
+        if (count) {
+            return;
+        }
+       }
+ 
     // Create a method that outputs the following for each number from 1
     // to whatever the user entered.
+    public static void outputs(Boolean count) {
 
     // If the number is evenly divisible by 5, output, "BAISD "
+    if (count % 5){
+        System.out.println("BAISD");
+    }
     // If the number is evenly divisible by 3, output, "is awesome!"
+    if (count % 3){
+        System.out.println("is awesome!");
+    }
     // If the number is evenly divisible by 3 and 5, output, "BAISD is awesome!"
+    if (count % 3 && count % 5){
+        System.out.println("BASID is awesome!");
+    }
+}
 
 }
 
