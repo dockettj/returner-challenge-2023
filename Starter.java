c50386dcbd446b347c1025b908e64ae107f463b0
Nwoodward2906 <000193486@student.baisd.net>
2023-02-10 13:21:32 -0500

    hello

--- Starter.java
+++ Starter.java
@@ -3,26 +3,34 @@ import java.util.Scanner;
 public class Starter {
     public static void main(String args[]) {
         // Variables and scanner
+        Scanner kb = new Scanner(System.in);
 
+        int count;
         // Ask the user, "How high do you want to count?" and get their input
+        System.out.println("How high do you want to count?");
+        count = kb.nextInt();
 
-        // Validate the input using a method
-        // If the input is not valid, ask the user,
-        // "That number is not valid. \nHow high do you want to count?"
+        while(Validator.validater(count) == false){
+            System.out.println("That number is not valid. \nHow high do you want to count?");
+            count = kb.nextInt();
+        }
 
-        // Use a method to output the solution
+        Validator.output(count);
+
     }
 
-    // Create a method that validates the input and returns true if valid
+}
 
+    // Validate the input using a method
+    // If the input is not valid, ask the user,
+    // "That number is not valid. \nHow high do you want to count?
+    // Use a method to output the solution
+    // Create a method that validates the input and returns true if valid
     // Create a method that outputs the following for each number from 1
     // to whatever the user entered.
-
     // If the number is evenly divisible by 5, output, "BAISD "
     // If the number is evenly divisible by 3, output, "is awesome!"
     // If the number is evenly divisible by 3 and 5, output, "BAISD is awesome!"
-
-}
 
 // Ways to stand out:
 
