f085a7effabbf50854b25ade1c8f4b7dda3c7906
Justin Kruskie <justin@jkruskie.com>
2023-02-14 21:58:16 -0500

    Jkruskie Returner Challenge

--- Starter.java
+++ Starter.java
@@ -3,14 +3,29 @@ import java.util.Scanner;
 public class Starter {
     public static void main(String args[]) {
         // Variables and scanner
+        Counter counter = new Counter();
+        Scanner input = new Scanner(System.in);
 
         // Ask the user, "How high do you want to count?" and get their input
+        System.out.print("How high do you want to count?: ");
 
-        // Validate the input using a method
-        // If the input is not valid, ask the user,
-        // "That number is not valid. \nHow high do you want to count?"
+        while (!input.hasNextInt()) {
+            // Validate the input
+            // If the input is not valid, ask the user,
+            // "That number is not valid. How high do you want to count?"
+                System.out.println("That number is not valid. \nHow high do you want to count?");
+                input.next();
+        }
 
-        // Use a method to output the solution
+        // Set the countTo variable in the Counter object
+        counter.setCountTo(input.nextInt());
+
+        // Close the scanner
+        input.close();
+
+        // Integer was validated
+        // Print the numbers
+        counter.printNumbers();
     }
 
     // Create a method that validates the input and returns true if valid
@@ -22,13 +37,37 @@ public class Starter {
     // If the number is evenly divisible by 3, output, "is awesome!"
     // If the number is evenly divisible by 3 and 5, output, "BAISD is awesome!"
 
+    private static class Counter {
+        // Count to variable
+        private int countTo;
+
+        // Constructor
+        public Counter() {
+            countTo = 0;
+        }
+
+        // Setters
+        public void setCountTo(int countTo) {
+            this.countTo = countTo;
+        }
+
+        public void printNumbers() {
+            // Print the numbers
+            for (int i = 1; i <= countTo; i++) {
+                // If the number is evenly divisible by 3 and 5, output, "BAISD is awesome!"
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.println("BAISD is awesome!");
+                // If the number is evenly divisible by 3, output, "is awesome!"
+                } else if (i % 3 == 0) {
+                    System.out.println("is awesome!");
+                // If the number is evenly divisible by 5, output, "BAISD "
+                } else if (i % 5 == 0) {
+                    System.out.println("BAISD");
+                // Otherwise, output the number
+                } else {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
 }
-
-// Ways to stand out:
-
-// Use a second class and static methods for validation and output or
-
-// Use an array to hold the numbers before printing them or
-
-// Use an array in an object to hold the numbers and use instance methods to
-// handle validation and output.
