ace2a446fbfab92458aaaf81ba643d8c2431166e
ShantiPalmer <112637754+ShantiPalmer@users.noreply.github.com>
2023-03-16 09:29:16 -0400

    Update Starter.java

--- Starter.java
+++ Starter.java
@@ -3,26 +3,74 @@ import java.util.Scanner;
 public class Starter {
     public static void main(String args[]) {
         // Variables and scanner
-
+       int userInput;
+        Scanner Keyboard;
         // Ask the user, "How high do you want to count?" and get their input
+        Keyboard.print("How high do you want to count?");
+        Keyboard.nextInt = userInput;
 
         // Validate the input using a method
+        if (userInput > 20)
+        {
+            Keyboard.print("That number is not valid. \nHow high do you want to count?");
+            Keyboard.nextint= userInput;
+        }
         // If the input is not valid, ask the user,
         // "That number is not valid. \nHow high do you want to count?"
 
         // Use a method to output the solution
+        String val;
+        String Output;
     }
 
-    // Create a method that validates the input and returns true if valid
+    // Create a method that validates the input and returns true if valid   
+    public static boolean val(int userInput)
+     while (userInput < 20)
+    {
+        return true;
+    }
+}
 
     // Create a method that outputs the following for each number from 1
     // to whatever the user entered.
+    public static String Output(int userInput){
+    int counter;
+    int one = 1;
+    int divisible;
+    while (counter < userInput){
+         Keyboard.print(counter);
+        String AWS;
+        String BAI;
+        String BAIAW;
+        int counter + int one;
+    }
+       
+    public static String BAI(int counter){
+
+         counter / 5 = int.class;
+         keyboard.print("BAISD");
+        }
+
+    public static String AWS(int counter){
+            
+            int counter / 3 = int.class;
+            print("is awsome");
+        }
+
+    public static String BAIAW(int counter){
+            
+          while (counter / 3 && counter / 5)
+            keyboard.print("BAISD is awesome");
+            
+        }
+
+    }
 
     // If the number is evenly divisible by 5, output, "BAISD "
     // If the number is evenly divisible by 3, output, "is awesome!"
     // If the number is evenly divisible by 3 and 5, output, "BAISD is awesome!"
+    
 
-}
 
 // Ways to stand out:
 
